8f8a9ac72bcc738e7169fd7b278aec430eb31012
ArsenKoch <arsenkocaran@icloud.com>
2022-11-02 23:20:35 +0300

    Create new class: ShopListRepositoryImpl.kt and added implementation of methods in this class

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElememt = getShopItem(shopItem.id)
+        shopList.remove(oldElememt)
+        shopList.add(shopItem)
+
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("No find $shopItemId this element!")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toMutableList()
+    }
+}
